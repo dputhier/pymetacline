7c3798721a9a9af47a8edc81a86fcea9878b4aaa
Your Name <you@example.com>
2019-03-04 15:41:06 +0100

    scripts/compute_tx_len.py to 755

--- scripts/compute_tx_len.py
+++ scripts/compute_tx_len.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-transcriptStart=dict()
-transcriptEnd=dict()
-
+transcriptStart = dict()
+transcriptEnd = dict()
 
 import re
 
-def GetTxGenomicLength(input_file = None):
 
-    fileHandler=open(input_file)
+def GetTxGenomicLength(input_file=None):
+    fileHandler = open(input_file)
     for line in fileHandler:
-        Token=line.split("\t")  
-        start=int(Token[3]) # le début de l'élément courant
-        end=int(Token[4]) # la fin de l'élément courant
+        Token = line.split("\t")
+        start = int(Token[3])  # le début de l'élément courant
+        end = int(Token[4])  # la fin de l'élément courant
         # L'identifiant du transcrit
 
-        txID=re.search('transcript_id "([^"]+)"', Token[8]).group(1)
+        txID = re.search('transcript_id "([^"]+)"', Token[8]).group(1)
 
         if txID not in transcriptStart:
 
-            transcriptStart[txID]=start
-            transcriptEnd[txID]=end
+            transcriptStart[txID] = start
+            transcriptEnd[txID] = end
 
         else:
-            if start < transcriptStart[txID]: 
 
-                transcriptStart[txID]= start
+            if start < transcriptStart[txID]:
+
+                transcriptStart[txID] = start
 
                 if end > transcriptEnd[txID]:
+                    transcriptEnd[txID] = end
 
-                    transcriptEnd[txID]= end
-
-            
     for txID in transcriptStart:
-
         print(txID + "\t" + str(transcriptEnd[txID] - transcriptStart[txID] + 1))
 
 
 if __name__ == '__main__':
-    GetTxGenomicLength(input_file = '../pymetacline/data/gtf/simple.gtf')
+    GetTxGenomicLength(input_file='../pymetacline/data/gtf/simple.gtf')
